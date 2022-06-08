24e596f9adcaea4e3d86533ed00304bee4e699af
KennetQ2903 <kennetquiroz@gmail.com>
2022-06-07 21:46:04 -0600

    first commit to twitter clone

---
+++ pages/timeline/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function  TimeLine(){
+    return (
+        <>
+            <h1>This is the timeline</h1>
+            <Link href='/'> go home</Link>
+        </>
+    )
+};
